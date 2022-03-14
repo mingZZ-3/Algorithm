58bec9ecaee616f90d4ac6be401a8d9d554248c3
Minji <alswl7754@gmail.com>
2022-03-14 21:38:32 +0900

    hash #2

--- python/algo_Hash.py
+++ python/algo_Hash.py
@@ -15,9 +15,18 @@ def hash_solution1(participant, completion):
     answer = dic[temp]
     return answer
 
-a = hash_solution1(["leo", "kiki", "eden"], ["eden", "kiki"])
-print(a)
 # ===== #2
+def solution(phone_book):
+    hash_map = {}
+    for phone_number in phone_book:
+        hash_map[phone_number] = 1
+    for phone_number in phone_book:
+        temp = ""
+        for num in phone_number:
+            temp += num
+            if temp in hash_map and temp != phone_number:
+                return False
+    return True
 
 # ===== #3
 
