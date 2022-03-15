949ecba9020a7a9a7b327b95a1008b376091247a
Minji <alswl7754@gmail.com>
2022-03-15 15:44:39 +0900

    hash #4

--- python/algo_Hash.py
+++ python/algo_Hash.py
@@ -39,3 +39,29 @@ def solution(clothes):
     return answer
 
 # ===== #4
+def solution(genres, plays):
+    dic_totall = {}
+    dic = {}
+    num = 0
+    answer = []
+
+    for gen in genres:
+        if gen not in dic_totall:
+            dic_totall[gen] = plays[num]
+        else:
+            dic_totall[gen] += plays[num]
+        num += 1
+
+    dic_totall = sorted(dic_totall.items(), key = lambda x: x[1], reverse=True)
+
+    for gen_totall, _ in dic_totall:
+        for gen in range(len(genres)):
+            if genres[gen] == gen_totall:
+                dic[gen] = plays[gen]
+        dic = sorted(dic.items(), key = lambda x: x[1], reverse=True)
+
+        for music, _ in dic[:2]:
+            answer.append(music)
+        dic = {}
+
+    return answer
