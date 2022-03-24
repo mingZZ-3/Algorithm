1c31fac7a2c290ff51d9a8a70936ab98ec7c0906
Minji <alswl7754@gmail.com>
2022-03-24 19:56:00 +0900

    Heap #2

--- python/heap.py
+++ python/heap.py
@@ -18,3 +18,22 @@ def solution(scoville, K):
         answer += 1
 
     return answer
+
+# ===== #2
+import heapq
+
+def solution(jobs):
+    tasks = sorted([(x[1], x[0]) for x in jobs],
+                   key=lambda x: (x[1], x[0]), reverse=True)
+    q = []
+    heapq.heappush(q, tasks.pop())
+    current_time, total_response_time = 0, 0
+    while len(q) > 0:
+        dur, arr = heapq.heappop(q)
+        current_time = max(current_time + dur, arr + dur)
+        total_response_time += current_time - arr
+        while len(tasks) > 0 and tasks[-1][1] <= current_time:
+            heapq.heappush(q, tasks.pop())
+        if len(tasks) > 0 and len(q) == 0:
+            heapq.heappush(q, tasks.pop())
+    return total_response_time // len(jobs)
