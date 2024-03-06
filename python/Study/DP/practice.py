80ee120408ff0c571570d90365624d780cd2cb16
minji <alswl7754@gmail.com>
2024-03-06 19:07:14 +0900

    최단 경로 알고리즘 : 다익스트라, 플로이드 워셜

--- python/Study/DP/practice.py
+++ python/Study/DP/practice.py
@@ -1,85 +1,102 @@
-# 1 - 개미 전사
-# n = int(input())
-# food = list(map(int, input().split()))
-#
-# d = [0] * 100 # dp 테이블 최기화
-#
-# # 바텀업 - DP
-# d[0] = food[0]
-# d[1] = max(food[0], food[1])
-# for i in range(2, n):
-#     d[i] = max(d[i-1], d[i-2] + food[i])
-#
-# print(d[n-1])
+##################################
+############### 1 - 개미 전사
+n = int(input())
+food = list(map(int, input().split()))
 
-# 2 - 1로 만들기
-# x = int(input())
-# d = [0]*30001
-#
-# # 바텀업
-# for i in range(2, x+1):
-#     # 현재 수에서 1을 빼는 경우
-#     d[i] = d[i-1] + 1
-#     if i % 2 == 0:
-#         d[i] = min(d[i], d[i//2] + 1)
-#     if i % 3 == 0:
-#         d[i] = min(d[i], d[i//3] + 1)
-#     if i % 5 == 0:
-#         d[i] = min(d[i], d[i//5] + 1)
-# print(d[x])
+d = [0] * 100 # dp 테이블 최기화
 
-# 3 - 효율적인 화폐 구성
-# n, m = map(int, input().split())
-# units = []
-# for i in range(n):
-#     units.append(int(input()))
-# d = [10001] * (m + 1)
-#
-# d[0] = 0
-# for i in range(n):
-#     for j in range(units[i], m + 1):
-#         if d[j - units[i]] != 10001:
-#             d[j] = min(d[j], d[j - units[i]] + 1)
-#
-# if d[m] == 10001:
-#     print(-1)
-# else:
-#     print(d[m])
+# 바텀업 - DP
+d[0] = food[0]
+d[1] = max(food[0], food[1])
+for i in range(2, n):
+    d[i] = max(d[i-1], d[i-2] + food[i])
 
-# 4 - 금광
-# for tc in range(int(input())):
-#     n, m = map(int, input().split())
-#     array = list(map(int, input().split()))
-#
-#     dp = []
-#     index = 0
-#     for i in range(n):
-#         dp.append(array[index:index+m])
-#         index += m
-#
-#     for j in range(1, m):
-#         for i in range(n):
-#             # 왼쪽 위
-#             if i == 0:
-#                 left_up = 0
-#             else:
-#                 left_up = dp[i-1][j-1]
-#             # 왼쪽
-#             left = dp[i][j-1]
-#             # 왼쪽 아래
-#             if i == n - 1:
-#                 left_bottom = 0
-#             else:
-#                 left_bottom = dp[i+1][j-1]
-#             dp[i][j] = dp[i][j] + max(left_up, left, left_bottom)
-#     result = 0
-#     for i in range(n):
-#         result = max(result, dp[i][m-1])
-#     print(result)
+print(d[n-1])
 
-# 5 - 병사 배치하기
+##################################
+############### 2 - 1로 만들기
+x = int(input())
+d = [0]*30001
+
+# 바텀업
+for i in range(2, x+1):
+    # 현재 수에서 1을 빼는 경우
+    d[i] = d[i-1] + 1
+    if i % 2 == 0:
+        d[i] = min(d[i], d[i//2] + 1)
+    if i % 3 == 0:
+        d[i] = min(d[i], d[i//3] + 1)
+    if i % 5 == 0:
+        d[i] = min(d[i], d[i//5] + 1)
+print(d[x])
+
+##################################
+############### 3 - 효율적인 화폐 구성
+n, m = map(int, input().split())
+units = []
+for i in range(n):
+    units.append(int(input()))
+d = [10001] * (m + 1)
+
+d[0] = 0
+for i in range(n):
+    for j in range(units[i], m + 1):
+        if d[j - units[i]] != 10001:
+            d[j] = min(d[j], d[j - units[i]] + 1)
+
+if d[m] == 10001:
+    print(-1)
+else:
+    print(d[m])
+
+##################################
+############### 4 - 금광
+for tc in range(int(input())):
+    n, m = map(int, input().split())
+    array = list(map(int, input().split()))
+
+    dp = []
+    index = 0
+    for i in range(n):
+        dp.append(array[index:index+m])
+        index += m
+
+    for j in range(1, m):
+        for i in range(n):
+            # 왼쪽 위
+            if i == 0:
+                left_up = 0
+            else:
+                left_up = dp[i-1][j-1]
+            # 왼쪽
+            left = dp[i][j-1]
+            # 왼쪽 아래
+            if i == n - 1:
+                left_bottom = 0
+            else:
+                left_bottom = dp[i+1][j-1]
+            dp[i][j] = dp[i][j] + max(left_up, left, left_bottom)
+    result = 0
+    for i in range(n):
+        result = max(result, dp[i][m-1])
+    print(result)
+
+##################################
+############### 5 - 병사 배치하기
 # 가장 긴 증가하는 부분 수열을 가장 긴 감소하는 수열로 활용해서 푸는 문제
 # LIS 점화식
 n = int(input())
 array = list(map(int, input().split()))
+# 순서를 뒤집어 '최장 증가 부분 수열' 문제로 변환
+array.reverse()
 
+# dp를 위한 1차원 dp 테이블 초기화
+dp = [1] * n
+
+# 가장 긴 증가하는 부분 수열(LIS) 알고리즘 수행
+for i in range(0, n):
+    for j in range(0, i):
+        if array[j] < array[i]:
+            dp[i] = max(dp[i], dp[j] + 1)
+# 열외해야 하는 병사의 최소 수를 출력
+print(n - max(dp))
---
+++ python/Study/최단 경로 문제/source.py
@@ -0,0 +1,148 @@
+#################################
+### 다익스트라 - 순차 탐색
+# 단계마다 방문하지 않은 노드 중에서 최단 거리가 가장 짧은 노드를 선택하기 위해 매 단계마다 1차원 테이블의 모든 원소를 확인
+# 노드 개수가 5,000개 이하일 때 가능
+
+INF = int(1e9) # 무한을 의미하는 값. 10억
+# 노드의 개수, 간선의 개수를 입력 받기
+n, m = map(int, input().split())
+# 시작 노드 번호를 입력 받기
+start = int(input())
+# 각 노드에 연결되어 있는 노드에 대한 정보를 담는 리스트 생성
+graph = [[] for i in range(n)]
+# 방문한 적 있는지 체크
+visited = [False] * (n + 1)
+# 최단 거리 테이블을 모두 무한으로 초기화
+distance = [INF] * (n + 1)
+
+# 모든 간선 정보 입력받기
+for i in range(m):
+    a, b, c = map(int, input().split())
+    # a번 노드에서 b번 노드로 가는 비용이 c
+    graph[a].append((b, c))
+
+# 방문하지 않은 노드 중에서, 가장 최단 거리가 짧은 노드의 번호 반환
+def get_smallest_node():
+    min_value = INF
+    index = 0 # 가장 최단 거리가 짧은 노드(인덱스)
+    for i in range(1, n + 1):
+        if distance[i] < min_value and not visited[i]:
+            min_value = distance[i]
+            index = i
+    return index
+
+def dijkstra(start):
+    # 시작 노드에 대해서 초기화
+    distance[start] = 0
+    visited[start] = True
+    for j in graph[start]:
+        distance[j[0]] = j[1]
+    # 시작 노드를 제외한 전체 n - 1개의 노드에 대해 반복
+    for i in range(n - 1):
+        now = get_smallest_node()
+        visited[now] = True
+        # 현재 노드와 연결된 다른 노드들 확인
+        for j in graph[now]:
+            cost = distance[now] + j[1]
+            # 현재 노드를 거쳐서 다른 노드로 이동하는 거리가 더 짧은 경우
+            if cost < distance[j[0]]:
+                distance[j[0]] = cost
+
+dijkstra(start)
+
+# 모든 노드로 가기 위한 최단 거리 출력
+for i in range(1, n + 1):
+    # 도달할 수 없는 경우, 무한이라고 출력
+    if distance[i] == INF:
+        print("INFINITY")
+    else:
+        print(distance[i])
+
+
+#################################
+### 다익스트라 - 우선순위 큐
+import heapq
+
+INF = int(1e9) # 무한을 의미하는 값. 10억
+# 노드의 개수, 간선의 개수를 입력 받기
+n, m = map(int, input().split())
+# 시작 노드 번호를 입력 받기
+start = int(input())
+# 각 노드에 연결되어 있는 노드에 대한 정보를 담는 리스트 생성
+graph = [[] for i in range(n+1)]
+# 방문한 적 있는지 체크
+visited = [False] * (n + 1)
+# 최단 거리 테이블을 모두 무한으로 초기화
+distance = [INF] * (n + 1)
+
+# 모든 간선 정보 입력받기
+for i in range(m):
+    a, b, c = map(int, input().split())
+    # a번 노드에서 b번 노드로 가는 비용이 c
+    graph[a].append((b, c))
+
+def dijkstra_heap(start):
+    q = []
+    # 시작 노드로 가기 위한 최단 거리는 0으로 설정, 큐에 삽입
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+    while q:
+        # 가장 최단 거리가 짧은 노드에 대한 정보 꺼내기
+        dist, now = heapq.heappop(q)
+        # 현재 노드가 이미 처리된 적이 있는 노드라면 무시
+        if distance[now] < dist:
+            continue
+        # 현재 노드와 연결된 다른 인접한 노드들을 확인
+        for i in graph[now]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                distance[0] = cost
+                heapq.heappush(q, (cost, i[0]))
+
+dijkstra_heap(start)
+
+# 모든 노드로 가기 위한 최단 거리 출력
+for i in range(1, n + 1):
+    # 도달할 수 없는 경우, 무한이라고 출력
+    if distance[i] == INF:
+        print("INFINITY")
+    else:
+        print(distance[i])
+
+#################################
+### 플로이드 워셜 알고리즘
+
+INF = int(1e9) # 무한을 의미하는 값. 10억
+
+# 노드의 개수 및 간선의 개수를 입력받기
+n, m = map(int, input().split())
+# 2차원 리스트(그래프 표현)를 만들고, 무한으로 초기화
+graph = [[INF] * (n + 1) for _ in range(n + 1)]
+
+# 자기 -> 자기 가는 비용은 0으로 초기화
+for a in range(1, n + 1):
+    for b in range(1, n + 1):
+        if a == b:
+            graph[a][b] = 0
+
+# 각 간선에 대한 정보를 입력 받아, 그 값으로 초기화
+for _ in range(m):
+    # a에서 b로 가는 비용은 c
+    a, b, c = map(int, input().split())
+    graph[a][b] = c
+
+# 점화식에 따라 플로이드 워셜 알고리즘 수행
+for k in range(1, n + 1):  # 거쳐가는 노드
+    for a in range(1, n + 1):  # 시작 노드
+        for b in range(1, n + 1):  # 도착 노드
+            graph[a][b] = min(graph[a][b], graph[a][k] + graph[k][b])
+
+# 수행된 결과 출력
+for a in range(1, n + 1):
+    for b in range(1, n + 1):
+        # 도달할 수 없는 경우, 무한으로 출력
+        if graph[a][b] == INF:
+            print("INFINITY", end=" ")
+        else:
+            print(graph[a][b], end=" ")
+    print()
