05a6013f129bad4312ab4974ef43ad0b9370333e
밍쯔 <82071369+mingZZ-3@users.noreply.github.com>
2024-06-01 15:06:05 +0900

    [Silver IV] Title: 로프, Time: 360 ms, Memory: 26772 KB -BaekjoonHub

---
+++ 백준/Silver/2217. 로프/로프.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        Integer[] ropes = new Integer[n];
+        int result = 0;
+        
+        for (int i = 0; i < n; i++) {
+            ropes[i] = Integer.parseInt(br.readLine());
+        }
+
+        Arrays.sort(ropes, Collections.reverseOrder());
+        for (int i = 0; i < n; i++) {
+            result = Math.max(result, ropes[i] * (i + 1));
+        }
+
+        System.out.print(result);
+    }
+}
