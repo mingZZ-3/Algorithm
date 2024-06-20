94f77b7ae29e52c40c6547fb837072bab5c46fdb
밍쯔 <82071369+mingZZ-3@users.noreply.github.com>
2024-06-20 17:27:37 +0900

    [Silver III] Title: 꿀 아르바이트, Time: 296 ms, Memory: 25340 KB -BaekjoonHub

---
+++ 백준/Silver/12847. 꿀 아르바이트/꿀 아르바이트.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        long[] incomes = new long[N + 1];
+
+        // 일급
+        st = new StringTokenizer(br.readLine());
+        incomes[0] = 0;
+        for (int i = 1; i < N + 1; i++) {
+            incomes[i] = incomes[i-1] + Long.parseLong(st.nextToken());
+        }
+
+        int startP = 0, endP = M;
+        long max_total = 0;;
+        while (endP <= N) {
+            long sum = incomes[endP] - incomes[startP];
+
+            if (sum > max_total) {
+                max_total = sum;
+            }
+
+            startP++;
+            endP++;
+        }
+        System.out.println(max_total);
+    }
+}
