e3c4783dbc43d2dab7c5b1234fb9bb846bbd3a5d
밍쯔 <82071369+mingZZ-3@users.noreply.github.com>
2024-06-27 19:24:30 +0900

    [level 2] Title: 올바른 괄호, Time: 21.61 ms, Memory: 52.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        Stack<Character> stack = new Stack<>();
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(s.charAt(i));
+            }
+            else {
+                if (!stack.empty() && stack.peek() == '(') {
+                    stack.pop();
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+        return stack.empty() ? true : false;
+    }
+}
