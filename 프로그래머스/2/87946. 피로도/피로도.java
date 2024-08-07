81f16e7566a83989e32fc62eaae83b3d32ba76ad
밍쯔 <82071369+mingZZ-3@users.noreply.github.com>
2024-08-07 16:41:44 +0900

    Update 피로도.java

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,6 +1,25 @@
 class Solution {
-    public int solution(int k, int[][] dungeons) {
-        int answer = -1;
-        return answer;
+    static boolean[] visited;
+    static int cnt;
+
+    public static int solution(int k, int[][] dungeons) {
+        cnt = 0;
+        visited = new boolean[dungeons.length];
+
+        dfs(0, k, dungeons);
+
+        return cnt;
+    }
+
+    private static void dfs(int depth, int k, int[][] dungeons) {
+        for (int i = 0; i < dungeons.length; i++) {
+            if (visited[i] || dungeons[i][0] > k) {
+                continue;
+            }
+            visited[i] = true;
+            dfs(depth + 1, k - dungeons[i][1], dungeons);
+            visited[i] = false;
+        }
+        cnt = Math.max(cnt, depth);
     }
 }
