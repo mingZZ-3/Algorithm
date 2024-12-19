2cc44912c0b944ebae52e978c54db264aabdeab9
밍쯔 <82071369+mingZZ-3@users.noreply.github.com>
2024-12-19 13:05:03 +0900

    [level 1] Title: 소수 만들기, Time: 26.50 ms, Memory: 93 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -1,37 +1,43 @@
 import java.util.*;
 
 class Solution {
+    List<Set<Integer>> combis = new ArrayList<>();
+    
     public int solution(int[] nums) {
         int answer = 0;
-        boolean[] primelist = isPrimeList(2997);
 
-        for(int i = 0; i < nums.length - 2; i++) {
-            for (int startp = i + 1; startp < nums.length - 1; startp++) {
-                int endp = startp + 1;
-                while (endp < nums.length){
-                    int sum = nums[i] + nums[startp] + nums[endp];
-                    if (primelist[sum]) answer++;
-                    endp++;
-                }
-            }
+        combi(nums, 0, new HashSet<>());
+
+        
+        for (Set<Integer> combi : combis) {
+            int sum = 0;
+            for (Integer n : combi) sum += n;
+            if (isPrime(sum)) answer++;
         }
 
         return answer;
     }
     
-    private boolean[] isPrimeList(int max) {
-        boolean[] isPrime = new boolean[max + 1];
-        Arrays.fill(isPrime, true);
-        isPrime[0] = isPrime[1] = false;
-
-        for (int i = 2; i * i <= max; i++) {
-            if (isPrime[i]) {
-                for (int j = i * i; j <= max; j += i) {
-                    isPrime[j] = false;
-                }
-            }
+    public void combi(int[] nums, int start, Set<Integer> curr) {
+        if (curr.size() == 3) {
+            combis.add(new HashSet<>(curr));
+            return;
         }
         
-        return isPrime;
+        for (int i=start; i<nums.length; i++) {
+            curr.add(nums[i]);
+            combi(nums, i+1, curr);
+            curr.remove(nums[i]);
+        }
+    }
+    
+    public boolean isPrime(int num) {
+        if (num < 2) return false;
+        
+        for (int i=2; i*i<= num; i++) {
+            if (num % i == 0)
+                return false;
+        }
+        return true;
     }
 }
