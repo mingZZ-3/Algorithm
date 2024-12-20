ecad921e3b93d34367775bd8db58c24049c7919b
밍쯔 <82071369+mingZZ-3@users.noreply.github.com>
2024-12-20 15:51:50 +0900

    [level 2] Title: 소수 찾기, Time: 13.31 ms, Memory: 90.5 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,45 +1,41 @@
 import java.util.*;
 
 class Solution {
-    Set<Integer> candidates = new HashSet<>();
+    Set<Integer> nums = new HashSet<>();
     
     public int solution(String numbers) {
-        int answer = 0;
-    
-        permutation(0, numbers, new boolean[numbers.length()], 0);
+        permutation(0, numbers, 0, new boolean[numbers.length()]);
         
-        System.out.println(candidates.toString());
-        for (int num : candidates) {
-            if (isPrime(num))
-                answer++;
+        int answer = 0;
+        for (int num : nums) {
+            if (isPrime(num)) answer++;
         }
         
         return answer;
     }
     
-    public void permutation(int curr, String numbers, boolean[] visited, int digit) {
-        if (digit == numbers.length()) {
-            return;
-        }
-        
-        for (int i = 0; i < numbers.length(); i++) {
-            if (visited[i]) continue;
-            
-            int newValue = curr + (int)((numbers.charAt(i) - '0')*Math.pow(10, digit));
-            candidates.add(newValue);
-            
-            visited[i] = true;
-            permutation(newValue, numbers, visited, digit + 1);
-            visited[i] = false;
-        }
-    }
-    
     public boolean isPrime(int n) {
         if (n < 2) return false;
         
-        for (int i=2; i*i <= n; i++) {
+        for (int i=2; i*i<=n; i++) {
             if (n % i == 0) return false;
         }
+        
         return true;
+    }
+    
+    public void permutation(int curr, String numbers, int digit, boolean[] visited) {
+        if (digit == numbers.length()) return;
+        
+        for (int i=0; i<numbers.length(); i++) {
+            if (visited[i]) continue;
+            
+            int newValue = curr + (int) ((numbers.charAt(i) - '0')*Math.pow(10, digit));
+            nums.add(newValue);
+            
+            visited[i] = true;
+            permutation(newValue, numbers, digit+1, visited);
+            visited[i] = false;
+        }
     }
 }
